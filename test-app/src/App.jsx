c17bad32240edb0349b2bd0eb999c650f97a0f84
alexeyaleksandrov <chinok73@yandex.ru>
2025-11-05 12:39:15 +0300

    PR-1

--- test-app/src/App.jsx
+++ test-app/src/App.jsx
@@ -1,12 +1,33 @@
 // src/App.jsx
 import './App.css';
 import Greeting from './Greeting'; // импортируем наш компонент
+import UserCard from "./UserCard.jsx";
+import TaskList from "./TaskList.jsx";
 
 function App() {
     return (
         <div className="App">
             {/* используем компонент как HTML-тег */}
             <Greeting />
+
+            {/* Используем UserCard и передаём ему props */}
+            <UserCard
+                name="Иван Иванов"
+                role="Администратор"
+                avatarUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfVMhpKmVy_-iwfRLAiNiaDslMa-2oEz7KTw&s"
+                isOnline={true}
+            />
+
+            {/* Используем UserCard и передаём ему props */}
+            <UserCard
+                name="Пётр Петров"
+                role="Менеджер"
+                avatarUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfVMhpKmVy_-iwfRLAiNiaDslMa-2oEz7KTw&s"
+                isOnline={false}
+            />
+
+            {/* добавляем компонент списка задач */}
+            <TaskList />
         </div>
     );
 }
